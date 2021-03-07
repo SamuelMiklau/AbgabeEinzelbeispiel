b35f61133e9c8e6cbd65beb2c6c86666dc9d78e4
SamuelMiklau <63232105+SamuelMiklau@users.noreply.github.com>
2021-03-07 16:47:18 +0100

    Designed App
    Build onClickListeners for Buttons

--- app/src/main/java/com/example/abgabe/MainActivity.java
+++ app/src/main/java/com/example/abgabe/MainActivity.java
@@ -3,12 +3,45 @@ package com.example.abgabe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button bttnSend;
+    EditText txtMatrklNr;
+    TextView txtAntwVomServer;
+    TextView txtAntwort;
+    Button bttnCalc;
+    TextView txtOrder;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bttnSend = (Button) findViewById(R.id.bttnSend);
+        bttnCalc = (Button) findViewById(R.id.bttnCalc);
+        txtMatrklNr = (EditText) findViewById(R.id.txtMatrklNr);
+        txtAntwVomServer = (TextView) findViewById(R.id.txtAntwVomServer);
+        txtAntwort = (TextView) findViewById(R.id.txtAntwort);
+        txtOrder = (TextView) findViewById(R.id.txtOrder);
+
+        bttnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        bttnCalc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
